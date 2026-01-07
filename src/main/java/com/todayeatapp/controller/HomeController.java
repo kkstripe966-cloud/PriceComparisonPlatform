3d2e7579fa8d7c0600d72c7400381bcf78578fbc
stripe <kkstripe966@gmail.com>
2026-01-07 10:36:14 +0800

    fix: 修复跨域配置支持前端5173端口
    - 修改HomeController的@CrossOrigin注解
    - 移除通配符'*'，改为允许前端端口5173
    - 支持allowCredentials以兼容认证请求
    - 为与前端联调做准备

--- src/main/java/com/todayeatapp/controller/HomeController.java
+++ src/main/java/com/todayeatapp/controller/HomeController.java
@@ -13,7 +13,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/api/home")
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "http://localhost:5173", allowCredentials = "true")
 public class HomeController {
 
     @Autowired
